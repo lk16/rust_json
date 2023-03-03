8aab43b64972a7d7d3757367ea7c625aa3f5a1b7
Luuk Verweij <luuk_verweij@msn.com>
2023-03-03 12:27:36 +0100

    feat: Tokenize null

--- README.md
+++ README.md
@@ -4,3 +4,35 @@
 This package was created to remember how rust packaging works.
 The actual code inside this project is probably not rocket science.
 
+---
+
+### Setup and run project
+
+```sh
+# Create project skeleton in a new folder `rust_playground`
+cargo new rust_playground
+
+# compile and run code
+# -q is to silence build info output
+cargo run -q
+
+# compile and run code in separate commands
+cargo build
+./target/debug/rust_playground
+```
+
+---
+
+### Tools used while developing
+
+```rs
+// Tell compiler to not complain about
+// unused code or variables in this crate
+#![allow(dead_code)]
+#![allow(unused_variables)]
+```
+
+```sh
+# Watch rust compiler output in separate shell while editing
+watch -cn 1 -- cargo --color=always run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,104 @@
+// TODO remove when done
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use std::fmt;
+
+#[derive(Debug)]
+enum TokenType {
+    Null,
+    True,
+    False,
+    String,
+    Comma,
+    Colon,
+    ObjectStart,
+    ObjectEnd,
+    ArrayStart,
+    ArrayEnd,
+}
+
+struct Token<'a> {
+    type_: TokenType,
+    value: &'a str,
+}
+
+impl<'a> fmt::Display for Token<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "type={:?} value={}", self.type_, self.value)
+    }
+}
+
+impl<'a> Token<'a> {
+    fn new(type_: TokenType, value: &'a str) -> Self {
+        Self { type_, value }
+    }
+
+    fn len(&self) -> usize {
+        self.value.len()
+    }
+}
+
+struct Tokenizer<'a> {
+    input: &'a str,
+    offset: usize,
+    tokens: Vec<Token<'a>>,
+}
+
+impl<'a> Tokenizer<'a> {
+    fn new(input: &'a str) -> Self {
+        Self {
+            input,
+            offset: 0,
+            tokens: vec![],
+        }
+    }
+
+    fn tokenize(mut self) -> Result<Vec<Token<'a>>, usize> {
+        loop {
+            let c = self.input.chars().nth(self.offset);
+
+            let token_result = match c {
+                None => break,
+                Some('n') => self.tokenize_null(),
+                Some(_) => todo!(),
+            };
+
+            match token_result {
+                Ok(token) => {
+                    self.offset += token.len();
+                    self.tokens.push(token);
+                }
+                Err(offset) => return Err(offset),
+            }
+        }
+
+        Ok(self.tokens)
+    }
+
+    fn tokenize_null(&self) -> Result<Token<'a>, usize> {
+        if self.input.starts_with("null") {
+            let token = Token::new(TokenType::Null, "null");
+            return Ok(token);
+        }
+        Err(self.offset)
+    }
+}
+
+fn tokenize(input: &str) -> Result<Vec<Token>, usize> {
+    Tokenizer::new(input).tokenize()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let tokenized = tokenize("null");
+
+    match tokenized {
+        Ok(tokens) => {
+            println!("Got {} token(s)", tokens.len());
+            for token in tokens.iter() {
+                println!("- {}", token);
+            }
+        }
+        Err(offset) => println!("Parse Error at offset {}", offset),
+    }
 }
